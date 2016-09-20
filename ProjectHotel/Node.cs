9a88c7d47d51684fe328fd0dfbb44b0ccc50f579
Danilo Kallan <danilokallan@hotmail.com>
2016-09-20 14:33:30 +0200

    Wat facilities toegevoegd aan de Node Class Persoon Class toegevoegd

--- ProjectHotel/Node.cs
+++ ProjectHotel/Node.cs
@@ -8,12 +8,50 @@ namespace ProjectHotel
 {
     public abstract class Node
     {
-        public Dictionary<Node, int> buren; //Dictionary met de Buren en de Tijd die het kost om daar naartoe te gaan.
+        Dictionary<Node, int> buren; //Dictionary met de Buren en de Tijd die het kost om daar naartoe te gaan.
+        Gang gang;
 
         public Node()
         {
             buren = new Dictionary<Node, int>();
         }
+    }
+
+    public class Kamer : Node
+    {
+        public Gang gang;
+
+        public Kamer()
+        {
+            
+        }
+    }
+
+    public class Gang : Node
+    {
+        public Dictionary<Node, int> buren;
+
+        public Gang() :base()
+        {
+
+        }
+    }
+
+    public class Restaurant : Node
+    {
+        public Gang gang;
+        
+
+        public Restaurant()
+        {
+           
+        }
+    }
+
+    public class Lift : Node
+    {
+        List<Persoon> mensen; //moeten allemaal verplaatsen als de lift dat doey.
+
     }
 
 }
