d632f8ecd0feca4e50b6a825e7a53cef15c0973f
Danilo Kallan <danilokallan@hotmail.com>
2016-09-20 14:15:34 +0200

    Abstract Class Node

--- ProjectHotel/Node.cs
+++ ProjectHotel/Node.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace ProjectHotel
 {
-    class Node
+    public abstract class Node
     {
+        public Dictionary<Node, int> buren; //Dictionary met de Buren en de Tijd die het kost om daar naartoe te gaan.
+
+        public Node()
+        {
+            buren = new Dictionary<Node, int>();
+        }
     }
+
 }
